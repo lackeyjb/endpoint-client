384f2b736f6601a90bc4f8109bd39b1bff2a1fbd
Bryan Lackey <lackeyjb1@gmail.com>
2016-02-05 22:45:25 -0500

    Add logout action creators

--- app/actions/index.js
+++ app/actions/index.js
@@ -2,6 +2,9 @@ export const LOGIN_REQUEST = 'LOGIN_REQUEST';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAILURE = 'LOGIN_FAILURE';
 
+export const LOGOUT_REQUEST = 'LOGOUT_REQUEST';
+export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
+
 export function requestLogin(creds) {
   return {
     type: LOGIN_REQUEST,
@@ -26,5 +29,21 @@ export function loginError(message) {
     isFetching: false,
     isAuthenticated: false,
     message,
+  };
+}
+
+export function requestLogout() {
+  return {
+    type: LOGOUT_REQUEST,
+    isFetching: true,
+    isAuthenticated: true,
+  };
+}
+
+export function receiveLogout() {
+  return {
+    type: LOGOUT_SUCCESS,
+    isFetching: false,
+    isAuthenticated: false,
   };
 }
